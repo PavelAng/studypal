c841588ba2fafc5b373786a4facba65487d2e6c4
PavelAng <pavka999@abv.bg>
2023-04-24 17:38:00 +0300

    Zapochvanka
    aide zadaimo na proekta

---
+++ src/main/java/com/edu/ruse/studypal/StudypalApplication.java
@@ -0,0 +1,13 @@
+package com.edu.ruse.studypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudypalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/ruse/studypal/StudypalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ruse.studypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudypalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
